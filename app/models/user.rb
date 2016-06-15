c616442dc98bbd6cf7f0923994d5ad1449a466c0
Mitchell Gu <me@mitchgu.com>
2016-06-15 00:35:44 -0400

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
+  validates_format_of :username, with: /\A[A-Za-z0-9\.\-\_]{3,50}\z/
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /\A.+@.+\..+\z/
+  validates :password, presence: true, length: { in: 6..50 }, confirmation: true
   has_secure_password
 
   def is_superuser
