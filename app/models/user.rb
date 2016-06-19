f46afddd392574c181d7d26e750a4094fcb15007
Mitchell Gu <me@mitchgu.com>
2016-06-19 19:13:15 -0400

    downcase user emails before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
   validates_format_of :username, with: /\A[A-Za-z0-9\.\-\_]{3,50}\z/
   validates :email, presence: true, uniqueness: true
